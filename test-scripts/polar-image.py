e301ca69d12ef7b2271ea21ff285d2ce3f663064
Harry Zuzan <iliketobicycle@gmail.com>
2016-04-26 19:30:39 -0700

    revising polar-image script

--- test-scripts/polar-image.py
+++ test-scripts/polar-image.py
@@ -1,6 +1,22 @@
 import h5py, numpy
 from PIL import Image
 
+# some definitions to get things going to a proper script
+
+# tall and skinny because the arc is longer than the radius
+POLAR_ROWS = 256
+POLAR_COLS = 128
+
+# go out this far to the edge given a unit radius
+MAX_POLAR_RADIUS = 0.85
+
+# each rotation samples out and around
+RADIUS_SAMPLES = 8
+ROTATION_SAMPLES = 8
+
+
+
+
 # the image to work on
 hfname = 'test-polar/raw-stack00000.hdf5'
 hfile = h5py.File(hfname,'r')
@@ -11,65 +27,58 @@ hfile.close()
 img = numpy.abs(img)**2
 img[0,0] = img.min()
 
-# centre the image
-img = numpy.roll(img,img.shape[0]/2,0)
-img = numpy.roll(img,img.shape[1]/2,1)
+# in case the image needs to be recentered.
+# for the Fourier transform the centering will be off by half a row
+def recentre_image(the_img):
+	the_img = numpy.roll(the_img,the_img.shape[0]/2,0)
+	the_img = numpy.roll(the_img,the_img.shape[1]/2,1)
+	return the_img
 
-#img[:,:] = 0.0
-#img[:,:144] = 1.0
+def get_sub_radii(rad1,rad2,nrad):
+	# take the square root to sample mini-wedges of equal area
+	root_rad1,root_rad2 = numpy.sqrt(rad1),numpy.sqrt(rad2)
+	root_radii = numpy.linspace(root_rad1,root_rad2,nrad+1)
+	radii = numpy.zeros((nrad,),numpy.double)
 
-# tall and skinny because the arc is longer than the radius
-polar_rows = 128
-polar_cols = 256
+	for k in range(nrad): radii[k] = 0.5*(root_radii[k] + root_radii[k+1])
 
-# go out this far to the edge given a unit radius
-cartesian_radius = 0.35
+	return radii*radii
 
-# each rotation samples out and around
-radius_samples = 8
-rotation_samples = 8
-max_radius = cartesian_radius*(min(*img.shape)/2)
+def get_sampling_angles(nrows,nrot):
+	rbounds = numpy.linspace(0.0, 2.0*numpy.pi/nrows, nrot+1)
+	rotns = [(0.5*(rbounds[k] + rbounds[k+1])) for k in range(nrot)]
+	return numpy.array(rotns)
+
+
+img = recentre_image(img)
+
+max_radius = MAX_POLAR_RADIUS*(min(*img.shape)/2)
 
 print('max radius =', max_radius, 'pixels')
 
 # out to the max radius and around the pi
-radius_steps = numpy.linspace(0.0,max_radius,polar_cols+1)
-rotation_steps = numpy.linspace(0.0,2.0*numpy.pi,polar_rows+1)
+radius_steps = numpy.linspace(0.0,max_radius,POLAR_COLS+1)
+rotation_steps = numpy.linspace(0.0,2.0*numpy.pi,POLAR_ROWS+1)
 
-rotation_image = numpy.zeros((polar_rows,polar_cols),numpy.double)
+rotation_image = numpy.zeros((POLAR_ROWS,POLAR_COLS),numpy.double)
+
+sampling_angles = get_sampling_angles(POLAR_ROWS,POLAR_COLS)
+
+centre_x = img.shape[1]/2.0
+centre_y = img.shape[0]/2.0
+
 # start in the middle going out
-for pcol in range(polar_cols):
-	# inside and outside edge of the wedge
+for pcol in range(POLAR_COLS):
+	print('pcol =', pcol)
+
 	rad1,rad2 = radius_steps[pcol], radius_steps[pcol+1]
-	print('pcol,rad1 =', pcol,rad1)
-	
-	# take the square root to sample mini-wedges of equal area
-	root_rad1,root_rad2 = numpy.sqrt(rad1),numpy.sqrt(rad2)
+	radii = get_sub_radii(rad1,rad2,RADIUS_SAMPLES)
 
-	# radius of a mini-wedge in the square root world
-	span = (root_rad2 - root_rad1)/radius_samples
+	# go around the circle in uniform steps as with radii but no square root
+	for prow in range(POLAR_ROWS):
+		shift = 2.0*numpy.pi*numpy.float(prow)/numpy.float(POLAR_ROWS)
+		rotns = sampling_angles + shift
 
-	# step out half way into each mini wedge in the square root world
-	root_radii = numpy.zeros((radius_samples,),numpy.double)
-	for idx in range(radius_samples):
-		root_radii[idx] = root_rad1 + idx*span + 0.5*span
-
-	# these are the radii to step across
-	radii = root_radii**2
-
-	# go around the circle in uniform steps e as above but no square root
-	for prow in range(polar_rows):
-#		print('prow =', prow)
-		rot1,rot2 = rotation_steps[prow], rotation_steps[prow+1]
-		span = (rot2 - rot1)/rotation_samples
-
-		rotns = numpy.zeros((rotation_samples,),numpy.double)
-
-		for idx in range(rotation_samples):
-			rotns[idx] = rot1 + idx*span + 0.5*span
-
-		rotns += numpy.pi
-	
 		# now sample the wedge
 		pixel_sum = 0.0
 
@@ -77,23 +86,20 @@ for pcol in range(polar_cols):
 			for rotn in rotns:
 				x = radius*numpy.cos(rotn)
 				y = radius*numpy.sin(rotn)
-				crow = numpy.int(img.shape[0]/2 - y)
-				ccol = numpy.int(x + img.shape[0]/2)
+				crow = numpy.int(centre_x - y)
+				ccol = numpy.int(centre_y + x)
 				pixel_sum += img[crow,ccol]
 
-		pixel_sum /= radius_samples*rotation_samples
-		rotation_image[prow,pcol] = pixel_sum
+		pixel_sum /= RADIUS_SAMPLES*ROTATION_SAMPLES
+		rotation_image[POLAR_ROWS - prow - 1,pcol] = pixel_sum
 
 #		break
-
 #	break
 
 A = 255.0*img/img.max()
 B = A.copy().astype(numpy.uint8)
 I = Image.fromarray(B)
 I.save('junk-raw.png')
-
-pimg = rotation_image.copy()
 
 A = 255.0*rotation_image/rotation_image.max()
 
